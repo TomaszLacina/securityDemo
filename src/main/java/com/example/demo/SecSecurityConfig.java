71ea3ac95f8414378ca3c21e356c51f6b8ab3fb4
tomek <tomasz.lacina@coderslab.pl>
2023-07-01 21:14:19 +0200

    added swagger

--- src/main/java/com/example/demo/SecSecurityConfig.java
+++ src/main/java/com/example/demo/SecSecurityConfig.java
@@ -19,7 +19,7 @@ public class SecSecurityConfig {
         );
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home", "/test", "/logout").permitAll()
+                        .requestMatchers("/", "/home", "/test", "/logout", "/swagger-ui/*", "/v3/api-docs", "/v3/api-docs/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
